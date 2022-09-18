76cf2f6d3a61f13c2f06c6799f3cac5436493a17
Aditya Bisht <adityabisht02@gmail.com>
2022-09-18 20:34:22 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (88.00%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,25 +1,17 @@
 class Solution {
-    
-    //MEMOIZATION APPROACH
-    public int rec(int n,int []dp){
+    public int climbStairs(int n) {
         if(n<=2){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        
-        return dp[n]=rec(n-1,dp)+rec(n-2,dp);
-        
-    }
-    
-    public int climbStairs(int n) {
         int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
         
-        for(int i=0;i<dp.length;i++){
-            dp[i]=-1;
+        
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
         
-        return rec(n,dp);
+        return dp[n];
     }
 }
