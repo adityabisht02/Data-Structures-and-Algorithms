f3b82c75cc662bf39d369950513143246cd92fb7
Aditya Bisht <adityabisht02@gmail.com>
2022-09-27 12:35:55 +0530

    Time: 25 ms (5.71%), Space: 41.7 MB (5.92%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,18 +1,20 @@
 class Solution {
+    public int rec(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return rec(n-1)+rec(n-2);
+    }
+    
     public int fib(int n) {
-        if(n==0 ||n==1){
-            return n;
-        }
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
+       if(n==0 || n==1){
+           return n;           
+       }
         
-        
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        
-        return dp[n];
+       return rec(n);
         
     }
 }
