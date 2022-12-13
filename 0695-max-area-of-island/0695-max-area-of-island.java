5fd33a41f5e3d1a680a8f65d354733538819d484
Aditya Bisht <adityabisht02@gmail.com>
2022-12-14 02:58:16 +0530

    Time: 3 ms (88.11%), Space: 42.7 MB (82.48%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,48 +1,38 @@
 class Solution {
-   int areacount,maxarea=0;
-    
-   public void dfs(int grid[][],int visited[][],int r,int c){
-     if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || visited[r][c]==1 || grid[r][c]==0){
-         return  ;
-     }
-       
-       areacount++;
-       System.out.print(areacount+ " ");
-       visited[r][c]=1;
-       
-       
-       dfs(grid,visited,r-1,c);
+    int maxArea=0,area;
+    public void dfs(int grid[][],int visited[][],int r,int c){
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]==0 || visited[r][c]==1){
+            return ;
+        }
+        area++;
+        visited[r][c]=1;
         dfs(grid,visited,r+1,c);
-        dfs(grid,visited,r,c+1);
-        dfs(grid,visited,r,c-1);
-       
-   }    
-
-   public int maxAreaOfIsland(int[][] grid) {
-     
-       int i,j;
-       
-       //visited array to keep track of visited elements
-       int visited[][]=new int[grid.length][grid[0].length];
-       
-       
-       for(i=0;i<grid.length;i++){
-           for(j=0;j<grid[0].length;j++){
-               
-               //if element is 1 and not visited
-               if(grid[i][j]==1 && visited[i][j]==0){
-                   
-                   areacount=0;
-                   dfs(grid,visited,i,j);
-                   System.out.println();
-                   if(areacount>maxarea){
-                       maxarea=areacount;
-                   }
-               }
-           }
-       }
-       
-       return maxarea;
-       
+         dfs(grid,visited,r-1,c);
+         dfs(grid,visited,r,c+1);
+         dfs(grid,visited,r,c-1);
+        
+        
+    }
+    
+    public int maxAreaOfIsland(int[][] grid) {
+        
+        int visited[][]=new int [grid.length][grid[0].length];
+        
+        
+        for(int i=0;i<grid.length;i++){
+           
+            for(int j=0;j<grid[0].length;j++){
+                if(visited[i][j]!=1 && grid[i][j]==1){
+                     area=0;
+                    dfs(grid,visited,i,j);
+                    if(area>maxArea){
+                        maxArea=area;
+                    }
+                }
+            }
+        }
+        
+        return maxArea;
+        
     }
 }
