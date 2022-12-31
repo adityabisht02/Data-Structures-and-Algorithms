2d489ed650e019263d69b4c6bebf7c209f764337
Aditya Bisht <adityabisht02@gmail.com>
2023-01-01 01:26:56 +0530

    Time: 2 ms (66.38%), Space: 44.1 MB (82.41%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,24 +1,19 @@
 class Solution {
-    public void dfs(List<List<Integer>> paths,List<Integer> path,int graph[][],int current){
-        //add current node to path
-        path.add(current);
+    public void dfs(int graph[][],List<List<Integer>> paths,List<Integer> path,int node){
+        path.add(node);
         
-        //if last node
-        if(current==graph.length-1){
+        if(node==graph.length-1){
             paths.add(path);
-            return;
+            return ;
         }
-        
-        //run a loop to perform dfs and add to path list
-        for(int i=0;i<graph[current].length;i++){
-         dfs(paths,new ArrayList(path),graph,graph[current][i]);
-        }        
+        for(int i=0;i<graph[node].length;i++){
+            dfs(graph,paths,new ArrayList(path),graph[node][i]);
+        }
     }
     
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> paths=new ArrayList<>();
-        
-        dfs(paths,new ArrayList<>(),graph,0);
-        return paths;
+        List<List<Integer>> paths= new ArrayList<>();
+        dfs(graph,paths,new ArrayList<>(),0);
+            return paths;
     }
 }
