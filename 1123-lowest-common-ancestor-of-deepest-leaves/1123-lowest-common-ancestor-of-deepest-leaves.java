54bd194485af28d9c23c5ce349d06914da216c71
Aditya Bisht <adityabisht02@gmail.com>
2023-01-02 21:12:07 +0530

    Time: 1 ms (77.90%), Space: 42.2 MB (66.67%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -14,74 +14,28 @@
  * }
  */
 class Solution {
-    public int getHeight(TreeNode root){
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        int leftHeight=height(root.left);
+        int rightHeight=height(root.right);
+        
+        //current node is LCA
+        if(leftHeight==rightHeight){
+            return root;
+        }
+        else if(leftHeight>rightHeight){
+            return lcaDeepestLeaves(root.left);
+        }
+        
+        return lcaDeepestLeaves(root.right);
+    }
+    
+    public int height(TreeNode root){
         if(root==null){
             return 0;
         }
-        return 1+ Math.max(getHeight(root.left),getHeight(root.right));
-    }
-    
-    public boolean contains(TreeNode root,List<TreeNode> leaves){
-        
-        for(int i=0;i<leaves.size();i++){
-            if(root==leaves.get(i)){
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    public TreeNode lowestCommonAncestor(TreeNode root,List<TreeNode> leaves){
-        if(root==null || contains(root,leaves)){
-            return root;
-        }
-       TreeNode left= lowestCommonAncestor(root.left,leaves);
-    TreeNode right= lowestCommonAncestor(root.right,leaves);
-        
-        if(left==null){
-            return right;
-        }
-        if(right==null){
-            return left;
-        }
-        return root;
-    }
-    
-    public TreeNode lcaDeepestLeaves(TreeNode root) {
-        if(root.left==null && root.right==null){
-            return root;
-        }
-        //get height
-        int height=getHeight(root);
-        
-        
-        //create a queue for bfs to get leaf nodes
-        Queue<TreeNode> q=new LinkedList<>();
-        q.offer(root);
-        int currentlevel=0;
-        //list for storing leaves
-        List<TreeNode> leaves=new ArrayList<>();
-        
-        while(!q.isEmpty()){
-            currentlevel++;
-            
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode current=q.poll();
-                if(currentlevel==height){
-                    leaves.add(current);
-                }
-                if(current.left!=null){
-                    q.offer(current.left);
-                }
-                if(current.right!=null){
-                    q.offer(current.right);
-                }
-            }
-        }
-        
-        //now find lcs
-        
-        return lowestCommonAncestor(root,leaves);
+        return 1+ Math.max(height(root.left),height(root.right));
     }
 }
