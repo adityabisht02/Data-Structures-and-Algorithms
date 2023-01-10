6a6bb5cf4a2ffd1da5403206e8335c45192f219d
Aditya Bisht <adityabisht02@gmail.com>
2023-01-10 14:44:28 +0530

    Time: 1 ms (91.54%), Space: 43.9 MB (82.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,29 +1,46 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int rows[]=new int[matrix.length];
-        int columns[]=new int[matrix[0].length];
+        //space optimised approach
+        int col0=-1; //for checking if column 0 needs to be 0, for row0 use matrix[0][0]
         
-        //traverse the matrix and mark rows and columns where 0 has occured 
-        //we will mark using a -1
-        
+        //1st traversal
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    rows[i]=-1;
-                    columns[j]=-1;
+                    
+                    //for 0 th column
+                    if(j==0){
+                        matrix[i][0]=0;
+                        col0=0;
+                    }
+                    //for 0th row and every other row and column
+                    else{
+                        matrix[0][j]=0; //marking column
+                        matrix[i][0]=0; //marking row
+                    }
+                    
+                    
                 }
             }
         }
         
-        //now traverse matrix and for marked row and column put 0 in 1 iteration
         
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(rows[i]==-1 || columns[j]==-1)     //means that row/column has to be marked 0
-                {
+        //second traversal should be in reverse cause if we do from front then 0th row and column would change which would change the whole matrix
+        for(int i=matrix.length-1;i>=0;i--){
+            for(int j=matrix[i].length-1;j>=0;j--){
+                //check if 0th column
+                if(j==0){
+                    if(col0==0){
+                        matrix[i][j]=0;
+                    }
+                }                
+                
+               else if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
+        
+        
     }
 }
