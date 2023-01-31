85edadbe5fa113a230d61d344c622fa3031646e4
Aditya Bisht <adityabisht02@gmail.com>
2023-01-31 17:47:15 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (90.78%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -4,9 +4,9 @@ class Solution {
         
         int ans[][]=new int[n][m];
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[j][i]=matrix[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[i][j]=matrix[j][i];
             }
         }
         
