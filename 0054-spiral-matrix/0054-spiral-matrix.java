abae7fc4d3a4f14eee7df740bbe6bcad26137ae2
Aditya Bisht <adityabisht02@gmail.com>
2023-03-24 12:11:41 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (21.73%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,19 +1,11 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-
-        int left=0,right=matrix[0].length-1,bottom=matrix.length-1,top=0;
         List<Integer> ans=new ArrayList<>();
-        int n=matrix.length*matrix[0].length;
         
-        
-        //in the loop below if top>bottom or left>right break immediately dont let other for loops execute cause it may lead to addition of \
-        // extra elements
-        
-        
+        int left=0,right=matrix[0].length-1,top=0,bottom=matrix.length-1;
         
         while(true){
-            
-            //left to right
+            //go from left to right
             for(int i=left;i<=right;i++){
                 ans.add(matrix[top][i]);
                 
@@ -21,10 +13,9 @@ class Solution {
             top++;
             if(top>bottom)break;
             
-            //top to bottom
+            //go top to bottom
             for(int i=top;i<=bottom;i++){
                 ans.add(matrix[i][right]);
-                
             }
             right--;
             if(left>right)break;
@@ -32,15 +23,12 @@ class Solution {
             //right to left
             for(int i=right;i>=left;i--){
                 ans.add(matrix[bottom][i]);
-                
             }
             bottom--;
             if(top>bottom)break;
             
-            //bottom to top
             for(int i=bottom;i>=top;i--){
                 ans.add(matrix[i][left]);
-               
             }
             left++;
             if(left>right)break;
