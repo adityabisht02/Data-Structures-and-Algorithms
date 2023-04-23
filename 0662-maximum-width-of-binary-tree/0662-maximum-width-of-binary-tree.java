de1de5ea53e2c8c1c4151a7548ae3f1856313c14
Aditya Bisht <adityabisht02@gmail.com>
2023-04-23 13:00:48 +0530

    Time: 2 ms (48.23%), Space: 42.4 MB (33.20%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -1,32 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        Map<TreeNode, Integer> m = new HashMap<TreeNode, Integer>();
-        q.offer(root);
-        m.put(root, 1);
-        int curW = 0;
-        int maxW = 0;
+        if(root==null)return 0;
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        HashMap<TreeNode,Integer> map=new HashMap<>();
+        map.put(root,0);
+        int maxWidth=0;
+        
         while(!q.isEmpty()){
-            int size = q.size();
-            int start = 0;
-            int end = 0;
-            for(int i = 0; i < size; i++){
-                TreeNode node = q.poll();
-                if(i == 0) start = m.get(node);
-                if(i == size - 1) end = m.get(node);
-                if(node.left != null){
-                    m.put(node.left, m.get(node) * 2);
-                    q.offer(node.left);
+            //get size of the current level
+            int size=q.size();
+            int leftMostIndex=0,rightMostIndex=0;
+            for(int i=0;i<size;i++){
+                TreeNode current=q.poll();
+                int index=map.get(current);
+                if(current.left!=null){
+                    q.add(current.left);
+                    int leftChildIndex=(2*index)+1-1;
+                    map.put(current.left,leftChildIndex);
+                    
                 }
-                if(node.right != null){
-                    m.put(node.right, m.get(node) * 2 + 1);
-                    q.offer(node.right);
+                if(current.right!=null){
+                    q.add(current.right);
+                    int rightChildIndex=(2*index)+2-1;
+                    map.put(current.right,rightChildIndex);
+                    
                 }
+                if(i==0){
+                    leftMostIndex=index;
+                }
+                if(i==size-1){
+                    rightMostIndex=index;
+                }
+              
             }
-            curW = end - start + 1;
-            maxW = Math.max(curW, maxW);
+              if(rightMostIndex-leftMostIndex+1>maxWidth){
+                    maxWidth=rightMostIndex-leftMostIndex+1;
+                }
         }
-        return maxW;
+        return maxWidth;
     }
 }
