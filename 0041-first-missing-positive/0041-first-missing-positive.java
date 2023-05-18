322f1e6fee5452f7dba1ff27a8e2f0ab5950438e
Aditya Bisht <adityabisht02@gmail.com>
2023-05-18 15:18:26 +0530

    Time: 1 ms (100.00%), Space: 50.7 MB (81.90%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,52 +1,48 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        // get rid of negative numbers and zero by replacing them with an already existing positive in array
-        // traverse array and mark n-1 th index as negative to mark nth element
-        // check if all present , if yes return nums.length+1 else return the missing num
-    
-    int positive=-1;
-        
-        //find positive
+        //get rid of negative numbers and zeroes
+        //first find any positive number present in nums and replace all zeroes and negatives with that positive
+        int num=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]>0){
-                positive=nums[i];
+                num=nums[i];
                 break;
             }
         }
-        //if no positive element
-        if(positive==-1){
+        //no positives found, return 1
+        if(num==0){
             return 1;
         }
         
-        //replace negatives and 0
+        //if positive found replace all zeroes and negatives with the positive
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<=0){
-                nums[i]=positive;
-            }
-        }
+         if(nums[i]<=0){
+             nums[i]=num;
+         }   
+        }     
         
+        //mark all positivr integers using indexes
         for(int i=0;i<nums.length;i++){
-            //mark at n-1 
-            int markingIndex=Math.abs(nums[i])-1;
-            
-            //if element hasnt been marked and is in array index range
-            if(nums[i]<=nums.length){
-                 if(nums[markingIndex]>0){
-                nums[markingIndex]=-nums[markingIndex];
-            }
+            int markingIndex=Math.abs(nums[i]);
+            //since we are marking on i-1
+            markingIndex--;  
+            //to mark eleemnt index that index should be in range of array and shouldnt be 0
+            //also element shouldnt be already marked
+            if(markingIndex>=0 && markingIndex<nums.length && nums[markingIndex]>0){
+                 nums[markingIndex]=-nums[markingIndex];
             }
            
-            
         }
         
-        //check indices to see which num is missing
+        //now find which index has not been marked
         for(int i=0;i<nums.length;i++){
+            //if index nit marked return that index
             if(nums[i]>0){
-                //we marked i-1 for n so return i+1
                 return i+1;
             }
         }
         
+        //if all marked
         return nums.length+1;
     }
 }
