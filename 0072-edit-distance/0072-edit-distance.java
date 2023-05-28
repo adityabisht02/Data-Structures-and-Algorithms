e974786e8b1e92447a8e96dbe3ea9f583cc6847a
Aditya Bisht <adityabisht02@gmail.com>
2023-05-28 23:44:02 +0530

    Time: 7 ms (28.37%), Space: 44.1 MB (7.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int getDist(String s1,String s2){
-       int dp[][]=new int[s1.length()+1][s2.length()+1];
-        //fill the array with starting values
+    public int minDistance(String word1, String word2) {
+        int dp[][]=new int[word1.length()+1][word2.length()+1];
+        //fill the first row and first column
         for(int i=0;i<dp.length;i++){
             dp[i][0]=i;
         }
@@ -9,21 +9,16 @@ class Solution {
             dp[0][i]=i;
         }
         
-        //now the tabulation part
         for(int i=1;i<dp.length;i++){
             for(int j=1;j<dp[0].length;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                dp[i][j]=dp[i-1][j-1];
-                }else{
-                    dp[i][j]=1+ Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+ Math.min(Math.min(dp[i-1][j-1],dp[i][j-1]),dp[i-1][j]);
                 }
             }
         }
         return dp[dp.length-1][dp[0].length-1];
-        
-    }
-    public int minDistance(String word1, String word2) {
-        
-        return getDist(word1,word2);
     }
 }
