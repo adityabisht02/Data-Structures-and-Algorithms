9c0a7dc92b432188346a0a2006ea64be6516d321
Aditya Bisht <adityabisht02@gmail.com>
2023-06-08 19:14:42 +0530

    Time: 3 ms (37.28%), Space: 43.8 MB (30.85%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,23 @@
 class Solution {
-    public void rec(List<List<Integer>> ans,List<Integer> ds,int arr[],int index,int target){
-        if(index>=arr.length){
-           if(target==0){
-               ans.add(new ArrayList<>(ds));
-               
-           }return ;
+    public void rec(List<List<Integer>> ans, List<Integer> list,int arr[],int index,int target){
+        if(index>=arr.length || target<0){
+            return ;
         }
-       
-        //if less than target we add the element and go further
-        if(arr[index]<=target){
-            ds.add(arr[index]);
-            rec(ans,ds,arr,index,target-arr[index]);
-            ds.remove(ds.size()-1);
+        list.add(arr[index]);
+        if(arr[index]==target){
+            ans.add(new ArrayList(list));
+
         }
-        //arr[index]> target then move on to next element
-        rec(ans,ds,arr,index+1,target);
+        rec(ans,list,arr,index,target-arr[index]);
+        //go without added element
+        list.remove(list.size()-1);
+        rec(ans,list,arr,index+1,target);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
-        rec(ans,new ArrayList<>(),candidates,0,target);
         
+        rec(ans,new ArrayList<>(),candidates,0,target);
         return ans;
+        
     }
 }
