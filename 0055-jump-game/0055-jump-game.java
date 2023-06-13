bbbaf89b9cae5624862aef8ccf7a8815b6ff61d1
Aditya Bisht <adityabisht02@gmail.com>
2023-06-13 11:52:46 +0530

    Time: 490 ms (8.94%), Space: 44.1 MB (40.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,35 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        //go backwards from goal state and keep changing goal state 
-        int goalIndex=nums.length-1;
-        
-        for(int i=nums.length-2;i>=0;i--){
-            if(i+nums[i]>=goalIndex){
-                goalIndex=i;
-            }
+    public boolean rec(int nums[],int index,int dp[]){
+        if(index>=nums.length-1){
+            return true;
         }
-        
-        return (goalIndex==0);
+        if(dp[index]==1){
+            return true;
+        }
+        if(dp[index]==0){
+            return false;
+        }
+        int n=nums[index];
+        //run a loop to make all possible jumps
+        for(int i=1;i<=n;i++){
+            boolean reachedEnd=rec(nums,index+i,dp);
+            
+            if(reachedEnd){
+                dp[index]=1;
+                return true;
+            }
+            
+        }
+        //if we cant reach end from this index through any number of steps, make this index false
+        dp[index]=0;
+        return false;
+    }
+    public boolean canJump(int[] nums) {
+        //keeps track of whether we can reach end index from that particular index
+        int dp[]=new int[nums.length];
+        for(int i=0;i<dp.length;i++){
+            dp[i]=-1;
+        }
+        return rec(nums,0,dp);
     }
 }
