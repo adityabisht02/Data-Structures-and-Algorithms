02fea7a89f912d97b8862d9cd0c3281b21e3085e
Aditya Bisht <adityabisht02@gmail.com>
2023-06-22 15:04:44 +0530

    Time: 1 ms (75.07%), Space: 44.7 MB (48.76%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,46 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //space optimised approach
-        int col0=-1; //for checking if column 0 needs to be 0, for row0 use matrix[0][0]
+      int col0=-1;
         
-        //1st traversal
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+            for(int j=0;j<matrix[i].length;j++){
                 if(matrix[i][j]==0){
-                    
-                    //for 0 th column
+                    // if 0th column
                     if(j==0){
-                        matrix[i][0]=0;
                         col0=0;
+                        matrix[i][0]=0;
                     }
-                    //for 0th row and every other row and column
                     else{
-                        matrix[0][j]=0; //marking column
-                        matrix[i][0]=0; //marking row
+                        matrix[0][j]=0;
+                        matrix[i][0]=0;
                     }
-                    
-                    
                 }
             }
         }
         
-        
-        //second traversal should be in reverse cause if we do from front then 0th row and column would change which would change the whole matrix
         for(int i=matrix.length-1;i>=0;i--){
-            for(int j=matrix[i].length-1;j>=0;j--){
-                //check if 0th column
+            for(int j=matrix[0].length-1;j>=0;j--){
                 if(j==0){
                     if(col0==0){
                         matrix[i][j]=0;
                     }
-                }                
-                
-               else if(matrix[i][0]==0 || matrix[0][j]==0){
+                }  
+                else if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        
-        
     }
 }
