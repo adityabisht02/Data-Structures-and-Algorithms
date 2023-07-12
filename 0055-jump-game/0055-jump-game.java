57afb3aabef894ee6c7084e592ad3a72974bbe08
Aditya Bisht <adityabisht02@gmail.com>
2023-07-12 23:36:24 +0530

    Time: 432 ms (10.34%), Space: 44 MB (79.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,33 +3,27 @@ class Solution {
         if(index>=nums.length-1){
             return true;
         }
-        if(dp[index]==1){
-            return true;
-        }
-        if(dp[index]==0){
+        if(dp[index]!=-1){
+            if(dp[index]==1){
+                return true;
+            }
             return false;
         }
-        int n=nums[index];
-        //run a loop to make all possible jumps
-        for(int i=1;i<=n;i++){
-            boolean reachedEnd=rec(nums,index+i,dp);
-            
-            if(reachedEnd){
+        int length=nums[index];
+        
+        for(int i=1;i<=length;i++){
+            boolean res=rec(nums,index+i,dp);
+            if(res){
                 dp[index]=1;
                 return true;
             }
-            
         }
-        //if we cant reach end from this index through any number of steps, make this index false
         dp[index]=0;
         return false;
     }
     public boolean canJump(int[] nums) {
-        //keeps track of whether we can reach end index from that particular index
         int dp[]=new int[nums.length];
-        for(int i=0;i<dp.length;i++){
-            dp[i]=-1;
-        }
+        Arrays.fill(dp,-1);
         return rec(nums,0,dp);
     }
 }
