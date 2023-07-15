08549b9ee5518cc13f6d98bc3e7389b503739c95
Aditya Bisht <adityabisht02@gmail.com>
2023-07-15 09:52:57 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (62.97%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,22 +1,14 @@
 class Solution {
     public void rotate(int[][] matrix) {
-       //DO TRANSPOSE AND THEN ROWISE SWITCHING
-        //IN PLACE TRANSPOSE CAUSE ROWS AND COLUMNS ARE EQUAL
-        
-        
-        //transpose loop
         for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){  //j equal to i to prevent repeated swapping of already swapped nodes
+            for(int j=i;j<matrix[0].length;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
         
-        
-        //reversing rowwise
         for(int i=0;i<matrix.length;i++){
-            //inside every row we reverse rows using two pointers
             int low=0,high=matrix[i].length-1;
             
             while(low<high){
@@ -25,9 +17,7 @@ class Solution {
                 matrix[i][high]=temp;
                 low++;
                 high--;
-                
             }
         }
-        
     }
 }
