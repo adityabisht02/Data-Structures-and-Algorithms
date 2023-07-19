9d870b8e18c297a396f6af3c89b0cbdb5851c90b
Aditya Bisht <adityabisht02@gmail.com>
2023-07-19 19:40:46 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (76.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,23 @@
 class Solution {
-    
-    public int dfs(int m,int n,int r,int c,int dp[][]){
+    public int dfs(int r,int c,int m,int n,int dp[][]){
         if(r>=m || c>=n){
             return 0;
+        }
+        if(dp[r][c]!=-1){
+            return dp[r][c];
         }
         if(r==m-1 && c==n-1){
             return 1;
         }
-        if(dp[r][c]!=0){
-            return dp[r][c];
-        }        
-        return dp[r][c]=dfs(m,n,r+1,c,dp)+dfs(m,n,r,c+1,dp);
-        
+        return dp[r][c]=dfs(r+1,c,m,n,dp)+dfs(r,c+1,m,n,dp);
     }
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
-        
-        int paths=dfs(m,n,0,0,dp);
-        return paths;
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return dfs(0,0,m,n,dp);
     }
 }
