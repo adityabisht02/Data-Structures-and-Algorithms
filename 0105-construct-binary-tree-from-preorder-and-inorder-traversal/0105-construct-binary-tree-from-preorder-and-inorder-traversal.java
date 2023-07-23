42a75d438475fda853b75f05231b5bb623e34749
Aditya Bisht <adityabisht02@gmail.com>
2023-07-23 09:09:02 +0530

    Time: 6 ms (28.00%), Space: 43.5 MB (41.23%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,23 +14,27 @@
  * }
  */
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-    return helper(0, 0, inorder.length - 1, preorder, inorder);
-}
-
-public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-    if (preStart > preorder.length - 1 || inStart > inEnd) {
-        return null;
-    }
-    TreeNode root = new TreeNode(preorder[preStart]);
-    int inIndex = 0; // Index of current root in inorder
-    for (int i = inStart; i <= inEnd; i++) {
-        if (inorder[i] == root.val) {
-            inIndex = i;
+    public TreeNode rec(int preIndex,int inStart,int inEnd,int preorder[],int inorder[]){
+        if(preIndex>=preorder.length || inStart>inEnd){
+            return null;
         }
+        TreeNode newnode=new TreeNode(preorder[preIndex]);
+        //find left and right subtrees using inorder array
+        int index=0;
+        for(int i=inStart;i<=inEnd;i++){
+            if(inorder[i]==preorder[preIndex]){
+                index=i;
+                break;
+            }
+        }
+        //now call for left subtree and right subtree
+        newnode.left=rec(preIndex+1,inStart,index-1,preorder,inorder);
+        newnode.right=rec(preIndex+index-inStart+1,index+1,inEnd,preorder,inorder);
+        
+        return newnode;
+        
     }
-    root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-    root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
-    return root;
-}
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return rec(0,0,inorder.length-1,preorder,inorder);
+    }
 }
