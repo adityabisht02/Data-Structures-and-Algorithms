5a2c53613e0f375e3580c93dec192530d236fa62
Aditya Bisht <adityabisht02@gmail.com>
2023-07-26 14:56:27 +0530

    Time: 4 ms (76.15%), Space: 44.5 MB (32.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,57 +1,45 @@
 class Solution {
-    
-    public boolean cycleExists(List<List<Integer>> adj,int node,int pathVisited[],int visited[]){
+    public boolean dfs(List<List<Integer>> adj,int visited[],int node,int pathVisited[]){
+        if(visited[node]==1){
+            if(pathVisited[node]==1){
+                return true;
+            }
+            return false;
+        }
         visited[node]=1;
         pathVisited[node]=1;
         
         List<Integer> children=adj.get(node);
         for(int i=0;i<children.size();i++){
-            int child=children.get(i);
-            if(visited[child]==1){
-                if(pathVisited[child]==1){
-                    return true;
-                }
-            }
-            else{
-                boolean res=cycleExists(adj,child,pathVisited,visited);
-                if(res){
-                    return true;
-                }
+           int child=children.get(i);
+            if(dfs(adj,visited,child,pathVisited)){
+                return true;
             }
         }
-        //remove node from pathvisited since we are returning tp previous recursion stack
         pathVisited[node]=0;
         return false;
-    }    
-    
+    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj=new ArrayList<>();
+     List<List<Integer>> adj=new ArrayList<>();
         
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        
         for(int i=0;i<prerequisites.length;i++){
-            int arr[]=prerequisites[i];
-            int el1=arr[0];
-            int el2=arr[1];
-            //add edge
+            int temp[]=prerequisites[i];
+            int el1=temp[0];
+            int el2=temp[1];
             adj.get(el2).add(el1);
         }
         int visited[]=new int[numCourses];
-        int pathVisited[]=new int[numCourses];
         
-        //can be multiple components so
         for(int i=0;i<numCourses;i++){
-            if(visited[i]!=1){
-                if(cycleExists(adj,i,pathVisited,visited)){
-                    //if cycle exists we cannot complete courses
-                    return false;
-                }
+        if(visited[i]!=1){
+            if(dfs(adj,visited,i,new int[numCourses])){
+                return false;
             }
+        }    
         }
         return true;
-        
-        
     }
 }
