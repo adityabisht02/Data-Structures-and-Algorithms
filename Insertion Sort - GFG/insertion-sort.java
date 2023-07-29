26c7c7db3ad7ab12011ad684ec2600608a28143a
Aditya Bisht <adityabisht02@gmail.com>
2023-07-29 19:45:27 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.java
+++ Insertion Sort - GFG/insertion-sort.java
@@ -47,12 +47,12 @@ class Solution
   {
       for(int i=1;i<n;i++){
           int j=i;
+          
           while(j>0 && arr[j-1]>arr[j]){
               int temp=arr[j-1];
               arr[j-1]=arr[j];
               arr[j]=temp;
               j--;
-              
           }
       }
   }
