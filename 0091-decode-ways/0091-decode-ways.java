d080c4375ffafea9ea24787d5bfd15094ab3f917
Aditya Bisht <adityabisht02@gmail.com>
2023-08-09 17:39:11 +0530

    Time: 14 ms (5.40%), Space: 40.4 MB (94.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -9,12 +9,12 @@ class Solution {
         if(dp[index]!=0){
             return dp[index];
         }
-        int res=helper(s,index+1,dp);
-        if(index<s.length()-1 &&(s.charAt(index)=='1' || s.charAt(index)=='2' && s.charAt(index+1)<'7')){
-            res+=helper(s,index+2,dp);
+        int takeOne=helper(s,index+1,dp);
+        int takeTwo=0;
+        if(index<s.length()-1 && (s.charAt(index)=='1' || s.charAt(index)=='2' && s.charAt(index+1)<'7')){
+            takeTwo=helper(s,index+2,dp);
         }
-        return dp[index]=res;
-        
+        return dp[index]=takeOne+takeTwo;
     }
     public int numDecodings(String s) {
         if(s.length()==0){
