d38837fbb064fcfc56a2b0f4ed9a68f4fb27767c
Aditya Bisht <adityabisht02@gmail.com>
2023-08-23 20:59:53 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (82.86%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int helper(int n){
+    public int helper(int n,int dp[]){
         if(n<=1){
             return 1;
         }
+        if(dp[n]!=0){
+            return dp[n];
+        }
         int ans=0;
         for(int i=1;i<=n;i++){
-            ans+= helper(i-1)*helper(n-i);         
+            ans+= helper(i-1,dp)*helper(n-i,dp);         
         }
-        return ans;
+        return dp[n]=ans;
     }
     public int numTrees(int n) {
-        return helper(n);
+        int dp[]=new int[n+1];
+        return helper(n,dp);
     }
 }
