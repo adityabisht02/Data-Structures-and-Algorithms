494e00dd9708f5515d2f612e55aa1a4e0f854054
Aditya Bisht <adityabisht02@gmail.com>
2023-12-24 17:51:03 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -30,49 +30,27 @@ class Rat {
 // } Driver Code Ends
 
 
-// User function Template for Java
-
-// m is the given matrix and n is the order of matrix
 class Solution {
-    public static void helper(ArrayList<String> ans,int grid[][],int n,int r,int c,int visited[][],StringBuilder sb){
-        if(r>=n || c>=n || r<0 || c<0){
+    public static void helper(int arr[][],ArrayList<String> ans,String temp,int r,int c,int visited[][]){
+        if(r>=arr.length || c>=arr[0].length || c<0 || r<0 || visited[r][c]==1 || arr[r][c]==0){
             return ;
         }
-        if(grid[r][c]==0 || visited[r][c]==1){
+        if(r==arr.length-1 && c==arr[0].length-1){
+            ans.add(temp);
             return ;
         }
-        if(r==n-1 && c==n-1){
-            StringBuilder temp=new StringBuilder(sb);
-            ans.add(temp.toString());
-            return;
-        }
-       
-        
         visited[r][c]=1;
+        helper(arr,ans,temp+"D",r+1,c,visited);
+         helper(arr,ans,temp+"U",r-1,c,visited);
+          helper(arr,ans,temp+"R",r,c+1,visited);
+           helper(arr,ans,temp+"L",r,c-1,visited);
+           visited[r][c]=0;
         
-        
-        sb.append('U');
-        helper(ans,grid,n,r-1,c,visited,sb);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append('D');
-        helper(ans,grid,n,r+1,c,visited,sb);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append('R');
-        helper(ans,grid,n,r,c+1,visited,sb);
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append('L');
-        helper(ans,grid,n,r,c-1,visited,sb);
-        sb.deleteCharAt(sb.length() - 1);
-        
-        visited[r][c]=0;
     }
     public static ArrayList<String> findPath(int[][] m, int n) {
-        // Your code here
         ArrayList<String> ans=new ArrayList<>();
-        if(m[0][0]==0){
-            return ans;
-        }
-        helper(ans,m,n,0,0,new int[n][n],new StringBuilder());
+        int visited[][]=new int[n][n];
+        helper(m,ans,"",0,0,visited);
         return ans;
-    }  
+    }
 }
